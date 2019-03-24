84666313c59ef53a78e778c50b629ec362049366
AlenaShkr <elalshkr@gmail.com>
2019-03-24 19:29:35 +0300

    priority-queue: first commit

--- src/max-heap.js
+++ src/max-heap.js
@@ -2,15 +2,24 @@ const Node = require('./node');
 
 class MaxHeap {
 	constructor() {
-		
+		this.parentNodes = [];
+		this.root = null;
 	}
 
 	push(data, priority) {
 		
+		const node = new Node(data, priority);
+		
+		this.insertNode(node);	
+		this.shiftNodeUp(node);
 	}
 
 	pop() {
-		
+		if (this.root === null) return ; else {
+			return this.root;
+
+
+		}
 	}
 
 	detachRoot() {
@@ -22,22 +31,30 @@ class MaxHeap {
 	}
 
 	size() {
+		return this.parentNodes.length;
 		
 	}
 
 	isEmpty() {
+		return this.parentNodes.length === 0;
 		
 	}
 
 	clear() {
+		this.root = null;
+		this.parentNodes = [];
 		
 	}
 
 	insertNode(node) {
+		if (this.root === null) {
+			this.root = node;
+		}
 		
 	}
 
 	shiftNodeUp(node) {
+		
 		
 	}
 
--- src/node.js
+++ src/node.js
@@ -1,21 +1,71 @@
 class Node {
 	constructor(data, priority) {
+		this.data = data;
+		this.priority = priority;
+		this.parent = null;
+		this.left = null;
+		this.right = null;
+
 
 	}
 
 	appendChild(node) {
-
+		
+		if (!this.left) {
+			this.left = node;
+			this.left.parent = this;
+		} else if (!this.right) {
+			this.right = node;
+			this.right.parent = this;
+		} else return;
 	}
 
 	removeChild(node) {
+		
+		if (this.left === node) {
+			this.left.parent = null;
+			this.left = null;
+			
+		}  else
+		if (this.right === node) {
+			this.right.parent = null;
+			this.right = null;
+			
 
+		} else {new Error(error);
+		}
+			
 	}
 
 	remove() {
-
+		
+		if (this.parent === null) return;
+			
+		this.parent.removeChild(this);
+			 
+			  
+		
 	}
 
 	swapWithParent() {
+		
+		if (this.parent === null) return;
+		if(this.parent.parent.left === this.parent){
+			this.parent.parent.left = this;
+		} else if(this.parent.parent.right === this.parent){
+			this.parent.parent.right = this;
+		}
+		
+		let originalChild = this;
+		let originalParent = this.parent;
+		originalParent.parent = originalChild;
+		
+		this.parent = this.parent.parent;
+		if(this === this.parent.left){
+			this.left = this.parent;
+		} else if( this === this.parent.right){
+			this.right = this.parent;
+		}
 		
 	}
 }
